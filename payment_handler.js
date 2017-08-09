f46e55bce5f55cfaa8a68dc8a2fbc0d0e8c6b739
Jinho Bang <jinho.bang@samsung.com>
2017-08-09 19:27:12 +0900

    Update payment_handler.js

--- payment_handler.js
+++ payment_handler.js
@@ -1,3 +1,7 @@
 self.addEventListener('paymentrequest', function(e) {
+  e.respondWith({
+    methodName: "hello",
+    details: {}
+  });
   e.waitUntil(clients.openWindow("https://paytest.github.io"));
 });
