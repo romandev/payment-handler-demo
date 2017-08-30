f47f9444d759629f32e608662c1c50e93a9b63fe
Jinho Bang <jinho.bang@samsung.com>
2017-08-30 14:39:49 +0900

    Update payment_handler.js

--- payment_handler.js
+++ payment_handler.js
@@ -24,7 +24,24 @@ self.addEventListener('activate', function(e) {
 });
 
 self.addEventListener('fetch', function(e) {
-  console.log(e);
+if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            // Put a copy of the response in the runtime cache.
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
 });
 
 self.addEventListener('install', function(e) {
