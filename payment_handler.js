29978a88b0e0e7eaf9d57a2c018356474f44fdab
Jinho Bang <jinho.bang@samsung.com>
2017-08-30 14:37:27 +0900

    Update payment_handler.js

--- payment_handler.js
+++ payment_handler.js
@@ -11,7 +11,16 @@ const PRECACHE_URLS = [
 ];
 
 self.addEventListener('activate', function(e) {
-  console.log(e);
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', function(e) {
